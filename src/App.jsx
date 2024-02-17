38ef930e291ebeb05d12d5a29f6dab8432d8b19e
Kiverok <kiver566@ukr.net>
2024-02-17 12:56:35 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import Header from './components/Header'
+import { ways } from './data'
+
+function WayToTeach({ title, description }) {
+  return (
+<li>
+            <p>
+              <strong>{title}</strong>
+              {description}
+            </p>
+          </li>
+  )
+}
 
 export default function App() {
   return (
     <div>
     <Header />
     <main>
-      <h1>Hello React!!</h1>
+      <section>
+        <h3>Наш підхід до навчання</h3>
+
+        <ul>
+          <WayToTeach 
+          title={ways[0].title}
+          description={ways[0].description} />
+          <WayToTeach {... ways[1]} />
+          <WayToTeach {... ways[2]} />
+          <WayToTeach {... ways[3]} />
+        </ul>
+      </section>
     </main>
     </div>
   )
